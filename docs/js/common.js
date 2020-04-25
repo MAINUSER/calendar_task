9245707a67a03560f18e7f4608e416390c1123c0
MAINUSER <q2zx.xz2q@gmail.com>
2020-04-25 20:39:55 +0300

    j

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,7 +1,11 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue!'
+		message: 'Hello Vue!',
+		activeColor: 'red',
+		fontSize: 30,
+		dynamicId: 'test',
+		h: 20
 	}
 })
 
---
+++ docs/vuecomponents/test.js
@@ -0,0 +1,8 @@
+Vue.component('test-div', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<div>123</div>'
+  })
